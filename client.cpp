1aa66143bf324d3265aee9ad0ffc3fe2078e851b
LC <contact@louiscoulet.com>
2026-09-02 03:19:33 +0200

    complex input management

--- client.cpp
+++ client.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <thread>
+#include <string>
 #include <cstring>
+#include <cstdio>
+#include <mutex>
 #include <unistd.h>
+#include <termios.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
 int socket_fd;
+std::string current_input;
+std::mutex input_mutex;
+termios original_tty;
 
 void receive_messages() {
     char buffer[1024];
@@ -17,12 +24,35 @@ void receive_messages() {
             break;
         }
         buffer[bytes] = '\0';
-        std::cout << "> " << buffer << std::endl;
-        std::cout.flush();
+        
+        {
+            std::lock_guard<std::mutex> lock(input_mutex);
+            
+            // Erase the current line
+            std::cout << "\033[2K\r";
+            
+            // Print the received message
+            std::cout << buffer << std::endl;
+            
+            // Put back the prompt with the saved input
+            std::cout << "Typing: " << current_input;
+            std::cout.flush();
+        }
     }
 }
 
 int main(int argc, char* argv[]) {
+    // Disable buffering for stdout and stdin
+    std::setbuf(stdout, nullptr);
+    std::setbuf(stdin, nullptr);
+    
+    // Set terminal to non-canonical mode
+    termios tty;
+    tcgetattr(STDIN_FILENO, &tty);
+    original_tty = tty;  // Save original settings
+    tty.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &tty);
+    
     if (argc != 3) {
         std::cerr << "Usage: " << argv[0] << " <ip> <port>" << std::endl;
         return 1;
@@ -53,13 +83,42 @@ int main(int argc, char* argv[]) {
     std::thread recv_thread(&receive_messages);
     recv_thread.detach();
 
-    std::string message;
-    while (std::getline(std::cin, message)) {
-        if (!message.empty()) {
-            send(socket_fd, message.c_str(), message.length(), 0);
+    std::cout << "> ";
+    std::cout.flush();
+
+    while (true) {
+        char c;
+        if (read(STDIN_FILENO, &c, 1) == 1) {
+            if (c == '\n') {
+                {
+                    std::lock_guard<std::mutex> lock(input_mutex);
+                    if (!current_input.empty()) {
+                        // Erase the current line
+                        std::cout << "\033[2K\r";
+                        std::cout.flush();
+                        std::cout << current_input;
+
+                        send(socket_fd, current_input.c_str(), current_input.length(), 0);
+                        
+                        current_input.clear();
+                    }
+                }
+                std::cout << std::endl << "Typing: ";
+                std::cout.flush();
+            } else {
+                {
+                    std::lock_guard<std::mutex> lock(input_mutex);
+                    current_input += c;
+                }
+                std::cout << c;
+                std::cout.flush();
+            }
         }
     }
 
+    // Restore terminal settings
+    tcsetattr(STDIN_FILENO, TCSANOW, &original_tty);
+    
     close(socket_fd);
     return 0;
 }
